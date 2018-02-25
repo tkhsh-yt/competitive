84de1b710b67ef632577be0a329db1ddc0c46957
TAKAHASHI <takahashi@ipl.cs.uec.ac.jp>
2018-02-25 14:06:03 +0900

    Format

--- AtCoder/AGC021/go/A.go
+++ AtCoder/AGC021/go/A.go
@@ -15,9 +15,9 @@ func main() {
 		c0 := int(cs[i] - '0')
 		if i < len(cs)-1 {
 			c1 := int(cs[i+1] - '0')
-			if (c0-1) + 9 > c0+c1 {
-				cs[i] = rune((c0-1) + '0')
-				for j := i+1; j < len(cs); j++ {
+			if (c0-1)+9 > c0+c1 {
+				cs[i] = rune((c0 - 1) + '0')
+				for j := i + 1; j < len(cs); j++ {
 					cs[j] = '9'
 				}
 				break
@@ -25,11 +25,10 @@ func main() {
 		}
 	}
 
-		ans := 0
+	ans := 0
 	for i := 0; i < len(cs); i++ {
 		ans += int(cs[i] - '0')
 	}
 
 	fmt.Println(ans)
 }
-
